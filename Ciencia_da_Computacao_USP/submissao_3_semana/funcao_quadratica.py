04858e5bc4ba177cc1ef2df35e5a68f6e9e43c6f
Rogério Conceição <searcherba@gmail.com>
2020-01-25 00:04:23 -0300

    Update folders names

--- Ciencia_da_Computacao_USP/submissao_3_semana/funcao_quadratica.py
+++ Ciencia_da_Computacao_USP/submissao_3_semana/funcao_quadratica.py
@@ -4,17 +4,28 @@ a = float(input("Digite o valor de a: "))
 b = float(input("Digite o valor de b: "))
 c = float(input("Digite o valor de c: "))
 
-delta = b**2 - 4*a*c
+def delta (a, b, c):
+    return b**2 - 4*a*c
+
+def bhaskaraSoma(a, b, delta):
+    return (-b + math.sqrt(delta)) / (2*a)
+
+def bhaskaraSubtracao(a, b, delta):
+    return (-b - math.sqrt(delta)) / (2*a)
+
+def raizesReais():
+    return bhaskaraSoma(a, b, delta), bhaskaraSubtracao(a, b, delta)
+
+delta = delta(a, b, c)
 
 if(delta == 0):
-    x1 = (-b + math.sqrt(delta)) / (2*a)
+    x1 = bhaskaraSoma(a, b, delta)
     print("a raiz desta equação é", x1)
 else:
     if delta < 0:
         print("esta equação não possui raízes reais")
     else:
-        x1 = (-b + math.sqrt(delta)) / (2*a)
-        x2 = (-b - math.sqrt(delta)) / (2*a)
+        x1, x2 = raizesReais()
         if x1 <= x2 :
             print("as raízes da equação são", x1, "e", x2)
         else:
