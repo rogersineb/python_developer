dbbf0286087ed449ab1baee5b62e875f01b27e42
Rogério Conceição <searcherba@gmail.com>
2020-02-08 23:58:29 -0300

    Adding compara_assinatura() and avalia_textos() functions to COH-PIAH program

--- Ciencia_da_Computacao_USP/submissao_9_semana/coh_piah.py
+++ Ciencia_da_Computacao_USP/submissao_9_semana/coh_piah.py
@@ -69,15 +69,22 @@ def n_palavras_diferentes(lista_palavras):
 
 def compara_assinatura(as_a, as_b):
     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
-    pass
+    grau_similaridade = 0
+    i = 1
 
+    while i < 7:
+        grau_similaridade += (abs(as_a[i-1] - as_b[i-1])) / 6
+        i+=1
+
+    return round(grau_similaridade,2)
+    
 def calcula_assinatura(texto):
     tamanho_medio_palavra = calcula_tmp(texto)
     type_token = calcula_tt(texto)
     hapax_legomana = calcula_hl(texto)
     tamanho_medio_setenca = calcula_tms(texto)
-    complexo_setenca = 0
-    tamanho_media_frase = 0
+    complexo_setenca = calcula_cs(texto)
+    tamanho_media_frase = calcula_tmf(texto)
 
     return [tamanho_medio_palavra,
                                  type_token,
@@ -88,8 +95,20 @@ def calcula_assinatura(texto):
     ]
 
 def avalia_textos(textos, ass_cp):
-    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
-    pass
+    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n)
+        do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
+    assinaturas_textos = []
+    for texto in textos:
+        assinaturas_textos.append(calcula_assinatura(texto))
+
+    probabilidades_textos = []
+    for i in range(len(assinaturas_textos)):
+        probabilidade = compara_assinatura(assinaturas_textos[i], ass_cp)
+        probabilidades_textos.append([probabilidade, i+1])
+
+    return min(probabilidades_textos)[1]    
+
+
 
 def lista_frases(sentencas):
     conjunto_frases = []
@@ -131,25 +150,42 @@ def calcula_hl(texto):
 
 def calcula_tms(texto):
     sentencas = separa_sentencas(texto)
-    soma_sentenca = len(sentencas[0]) + len(sentencas[1])
+
+    soma_sentenca = 0
+    for sentenca in sentencas:
+        soma_sentenca += len(sentenca)
 
     return soma_sentenca / len(sentencas)
         
-def calcula_cs():
-    pass
+def calcula_cs(texto):
+    sentencas = separa_sentencas(texto)
+    frases = lista_frases(sentencas)
 
-def calcula_tmf():
-    pass
+    return len(frases) / len(sentencas)
+
+def calcula_tmf(texto):
+    frases = lista_frases(separa_sentencas(texto))
+    soma_caracteres_frases = 0
+    for frase in frases:
+        soma_caracteres_frases += len(frase)
+
+    return soma_caracteres_frases / len(frases)
 
 def test_calculo():
     texto = "Muito além, nos confins inexplorados da região mais brega da Borda Ocidental desta Galáxia, há um pequeno sol amarelo e esquecido. Girando em torno deste sol, a uma distancia de cerca de 148 milhões de quilômetros, há um planetinha verde-azulado absolutamente insignificante, cujas formas de vida, descendentes de primatas, são tão extraordinariamente primitivas que ainda acham que relógios digitais são uma grande ideia."
     assert calcula_assinatura(texto) == [5.571428571428571, 0.8253968253968254, 0.6984126984126984, 210.0, 4.5, 45.888888888888886]
 
 
+def test_compara_ass():
+    assert compara_assinatura([4.34, 0.05, 0.02, 12.81, 2.16, 0.0],[3.96, 0.05, 0.02, 22.22, 3.41, 0.0]) == 1.84
 
-
-
-
+def test_avalia_texto():
+    ass_cp =[4.79, 0.72, 0.56, 80.5, 2.5, 31.6]
+    textos = ["Navegadores antigos tinham uma frase gloriosa:\"Navegar é preciso; viver não é preciso\". Quero para mim o espírito [d]esta frase, transformada a forma para a casar como eu sou: Viver não é necessário; o que é necessário é criar. Não conto gozar a minha vida; nem em gozá-la penso. Só quero torná-la grande,ainda que para isso tenha de ser o meu corpo e a (minha alma) a lenha desse fogo. Só quero torná-la de toda a humanidade;ainda que para isso tenha de a perder como minha. Cada vez mais assim penso.Cada vez mais ponho da essência anímica do meu sangueo propósito impessoal de engrandecer a pátria e contribuirpara a evolução da humanidade.É a forma que em mim tomou o misticismo da nossa Raça.",
+              "Voltei-me para ela; Capitu tinha os olhos no chão. Ergueu-os logo, devagar, e ficamos a olhar um para o outro... Confissão de crianças, tu valias bem duas ou três páginas, mas quero ser poupado. Em verdade, não falamos nada; o muro falou por nós. Não nos movemos, as mãos é que se estenderam pouco a pouco, todas quatro, pegando-se, apertando-se, fundindo-se. Não marquei a hora exata daquele gesto. Devia tê-la marcado; sinto a falta de uma nota escrita naquela mesma noite, e que eu poria aqui com os erros de ortografia que trouxesse, mas não traria nenhum, tal era a diferença entre o estudante e o adolescente. Conhecia as regras do escrever, sem suspeitar as do amar; tinha orgias de latim e era virgem de mulheres.",
+              "NOSSA alegria diante dum sistema metafisico, nossa satisfação em presença duma construção do pensamento, em que a organização espiritual do mundo se mostra num conjunto lógico, coerente a harmônico, sempre dependem eminentemente da estética; têm a mesma origem que o prazer, que a alta satisfação, sempre serena afinal, que a atividade artística nos proporciona quando cria a ordem e a forma a nos permite abranger com a vista o caos da vida, dando-lhe transparência."]
+    
+    assert avalia_textos(textos, ass_cp) == 2
 
 
 
