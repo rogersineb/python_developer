4b65fa22d6de07cde3ec0984a7d832c53c4afc71
Rogério Conceição <searcherba@gmail.com>
2020-02-13 23:50:03 -0300

    Extra exercises

---
+++ Ciencia_da_Computacao_USP_2/submissao_1_semana/imprime_matriz.py
@@ -0,0 +1,8 @@
+def imprime_matriz(matriz):
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if j < len(matriz[i])-1:
+                print(matriz[i][j], end=" ")
+            else:
+                print(matriz[i][j], end="")
+        print()
